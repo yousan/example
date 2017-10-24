4a9db90d14f68cd6f9ac00bc83279f6ade5d45d8
Yousan_O <yousan@l2tp.org>
2017-10-24 21:28:37 +0900

    gj

--- hoge.js
+++ hoge.js
@@ -1,5 +1,4 @@
 ( function ($) {
-<<<<<<< HEAD
     var body = $('body'),
         _window = $(window);
 
@@ -19,25 +18,23 @@ nt').height() - secondary;
                 $('#colophon').css('margin-top', margin + 'px');
         }
     });
-=======
-	var body = $('body'),
-		_window = $(window);
+    var body = $('body'),
+        _window = $(window);
 
-	/**
-	 * Adds a top margin to the footer if the sidebar widget area is higher
-	 * than the rest of the page, to help the footer always visually clear
-	 * the sidebar.
-	 */
-	$(function () {
-		if (body.is('.sidebar')) {
-			var sidebar = $('#secondary .widget-area'),
-				secondary = ( 0 == sidebar.length ) ? -40 : sidebar.height(),
-				margin = $('#tertiary .widget-area').height() - $('#conte\
+    /**
+     * Adds a top margin to the footer if the sidebar widget area is higher
+     * than the rest of the page, to help the footer always visually clear
+     * the sidebar.
+     */
+    $(function () {
+        if (body.is('.sidebar')) {
+            var sidebar = $('#secondary .widget-area'),
+                secondary = ( 0 == sidebar.length ) ? -40 : sidebar.height(),
+                margin = $('#tertiary .widget-area').height() - $('#conte\
 nt').height() - secondary;
 
-			if (margin > 0 && _window.innerWidth() > 999)
-				$('#colophon').css('margin-top', margin + 'px');
-		}
-	});
->>>>>>> master
+            if (margin > 0 && _window.innerWidth() > 999)
+                $('#colophon').css('margin-top', margin + 'px');
+        }
+    });
 }
